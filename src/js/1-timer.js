bd87ba8756c3577cf379c0f7b46b81d480b09584
GiovanniAuditore <66073653+GiovanniAuditore@users.noreply.github.com>
2025-05-26 16:31:44 +0200

    upd

---
+++ src/js/1-timer.js
@@ -0,0 +1,34 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const startBtn = document.querySelector('[data-start]');
+const dateInput = document.getElementById('datetime-picker');
+let selectedDate = null;
+let intervalId = null;
+
+startBtn.disabled = true;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] <= new Date()) {
+      iziToast.error({
+        title: "Error",
+        message: "Please choose a date in the future",
+        position: "topRight",
+      });
+      startBtn.disabled = true;
+    } else {
+      selectedDate = selectedDates[0];
+      startBtn.disabled = false;
+    }
+  },
+};
+
+flatpickr(dateInput, options);
